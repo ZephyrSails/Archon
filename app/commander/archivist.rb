b1280e3f9a9a6e109744106d4abdf5c8dc09109b
ZephyrSails <zephyrsails@gmail.com>
2015-12-02 15:54:50 +0800

    another typo

--- app/commander/archivist.rb
+++ app/commander/archivist.rb
@@ -107,7 +107,7 @@ module Archivist
   # options = Archivist.options_idle(3000, 12750, 100, "1:410:4")
   # options = Archivist.optins_idle_gala("1", 100)
   # options[:is_safe_to_espionage?] = [true]
-  options = Archivist.options_idle_safe_to_espionage_unknow
+  # options = Archivist.options_idle_safe_to_espionage_unknow
   # positions = Archivist.get_positions(options)
   # Processor.instance.start
   # GeneralHelper.get_agent
