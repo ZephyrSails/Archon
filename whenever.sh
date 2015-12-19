e284c71c3fd82144390bbf5c61701dc8bb67c605
ZephyrSails <zephyrsails@gmail.com>
2015-12-19 22:51:52 +0800

    when

--- whenever.sh
+++ whenever.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
-if [ "$1" = "dev" ];then
+if [ "$1" = "reload" ];then
 echo "running: whenever --update-crontab --set environment='development'"
 whenever --update-crontab --set environment='development'
 echo "running: crontab -l"
 crontab -l
 fi
 
-if [ "$1" = "restart" ];then
-echo "running: whenever --update-crontab"
-whenever --update-crontab
-echo "running: crontab -l"
-crontab -l
-fi
+# if [ "$1" = "restart" ];then
+# echo "running: whenever --update-crontab"
+# whenever --update-crontab
+# echo "running: crontab -l"
+# crontab -l
+# fi
 
 if [ "$1" = "clear" ];then
 echo "running: whenever --update-crontab"
